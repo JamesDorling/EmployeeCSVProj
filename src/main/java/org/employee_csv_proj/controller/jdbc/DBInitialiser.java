d45465520c75640f66292d0350584b7ed599747c
mthomas1331 <mthomas6298@outlook.com>
2021-12-21 17:25:02 +0000

    DBInitialiser class uncommented to work with the added files

--- src/main/java/org/employee_csv_proj/controller/jdbc/DBInitialiser.java
+++ src/main/java/org/employee_csv_proj/controller/jdbc/DBInitialiser.java
@@ -1,31 +1,33 @@
 package org.employee_csv_proj.controller.jdbc;
 
+import org.employee_csv_proj.controller.jdbc.sql_queries.DbInitialiseStatements;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DBInitialiser {
     private static void initialiseCompanyDatabase() {
-        //try {
-        //    Connection connection = DBConnectionManager.dbInitialiseConnection();
-        //    connection.createStatement().executeUpdate(DbInitialiseStatements.DB_INITIALISE);
-        //    DBConnectionManager.closeConnection(connection);
-        //} catch (SQLException e) {
-        //    e.printStackTrace();
-        //}
+        try {
+            Connection connection = DBConnectionManager.dbInitialiseConnection();
+            connection.createStatement().executeUpdate(DbInitialiseStatements.DB_INITIALISE);
+            DBConnectionManager.closeConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void initialiseEmployeesTable() {
-        //try {
-        //    Connection connection = ConnectionManager.dbCompanyXConnection();
-        //    connection.createStatement().executeUpdate(DbInitialiseStatements.DB_INITIALISE_EMPLOYEES_TABLE);
-        //    ConnectionManager.closeConnection(connection);
-        //} catch (SQLException e) {
-        //    e.printStackTrace();
-        //}
+        try {
+           Connection connection = DBConnectionManager.dbCompanyXConnection();
+            connection.createStatement().executeUpdate(DbInitialiseStatements.DB_CREATE_EMPLOYEES_TABLE);
+            DBConnectionManager.closeConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void initialiseDB() {
-        //DbInitialiser.initialiseCompanyDatabase();
-        //DbInitialiser.initialiseEmployeesTable();
+        DBInitialiser.initialiseCompanyDatabase();
+        DBInitialiser.initialiseEmployeesTable();
     }
 }
