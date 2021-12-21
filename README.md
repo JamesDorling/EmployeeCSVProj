008f28ab0966234cf9fbea2f7e2f3a88773767df
JamesDorling <jamesrdorling@gmail.com>
2021-12-21 16:10:50 +0000

    Added config files for the database, and added initialisers for the database.

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ This is a group project to introduce us to working in a team and with databases.
 
 ## Table of Contents
 To be filled out when we realise what we are doing with the data/database.
+
+### How to use
+Firstly, to set up a database change the dummy config login and password details via this filepath:
+src/main/java/org/employee_csv_proj/config/configDummy.properties
+and copy that file to the same location with the name "config.properties".
--- src/main/java/org/employee_csv_proj/config/Config.java
+++ src/main/java/org/employee_csv_proj/config/Config.java
@@ -12,7 +12,7 @@ public class Config {
         config = new Properties();
 
         try {
-            config.load(new BufferedReader(new FileReader("config.properties")));
+            config.load(new BufferedReader(new FileReader("src/main/java/org/employee_csv_proj/config/config.properties")));
         } catch (IOException e) {
             //Log a failure
             e.printStackTrace();
@@ -21,5 +21,21 @@ public class Config {
 
     public static String employeeCSVFileLocation() {
         return config.getProperty("employee_csv_file_location");
+    }
+
+    public static String dbDatabaseConnectionURL() {
+        return config.getProperty("db_connection_url");
+    }
+
+    public static String dbUsername() {
+        return config.getProperty("username");
+    }
+
+    public static String dbPassword() {
+        return config.getProperty("password");
+    }
+
+    public static String dbDatabaseName() {
+        return config.getProperty("db_name");
     }
 }
